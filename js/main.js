1d2f46d18ddf55732505f05028e9b4aea02ddad3
Icia <icia83@gmail.com>
2018-04-22 15:26:37 +0200

    add styles

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function checkDataLocalStorage() {
 };
 
 // Call API
-function getPublicMember () {
+function getPublicMembers() {
   fetch('https://api.github.com/orgs/Adalab/public_members?per_page=68')
     .then(function(response) {
       return response.json();
@@ -60,7 +60,7 @@ function paintSelectValues() {
     option.innerHTML = ALL_MEMBERS[i].name || ALL_MEMBERS[i].login;
     select.appendChild(option);
 
-    select.addEventListener('change', showAdalaberCard)
+    select.addEventListener('change', showAdalaberCard);
   }
 }
 
@@ -71,38 +71,38 @@ function showAdalaberCard() {
   let adalaberCard = '';
 
   if(showAdalaber){
-    document.querySelector('.card__section').style.display = 'block';
+    document.querySelector('.adalaber__card').style.display = 'block';
     adalaberCard =
-      `<img class="" src="${ALL_MEMBERS[i].avatar_url} alt=${ALL_MEMBERS[i].login}">
-      <div>
+      `<img class="adalaber__photo" src="${ALL_MEMBERS[i].avatar_url} alt=${ALL_MEMBERS[i].login}">
+      <div class="adalaber__info">
         <span>@${ALL_MEMBERS[i].login}</span>
         <h2>${ALL_MEMBERS[i].name}</h2>
         <p><i class="fas fa-map-marker-alt"></i>${ALL_MEMBERS[i].location ? ALL_MEMBERS[i].location : '--' }</p>
       </div>
-      <div>
-        <div>
-          <p>${ALL_MEMBERS[i].public_repos} </p>
-          <p>Repositories</p>
+      <div class="repos">
+        <div class="repos__info">
+          <span>${ALL_MEMBERS[i].public_repos}</span>
+          <p>Repos</p>
         </div>
-        <div>
-          <p>${ALL_MEMBERS[i].followers} </p>
+        <div class="repos__info">
+          <span>${ALL_MEMBERS[i].followers}</span>
           <p>Followers</p>
         </div>
-        <div>
-          <p>${ALL_MEMBERS[i].following} </p>
+        <div class="repos__info">
+          <span>${ALL_MEMBERS[i].following}</span>
           <p>Following</p>
         </div>
       </div>
-    <div>
+    <div class="member__data">
       <p>Miembro desde ${new Date(ALL_MEMBERS[i].created_at).getFullYear()}</p>
     </div>`;
   }
   else {
-    document.querySelector('.card__section').style.display = 'none';
+    document.querySelector('.adalaber__card').style.display = 'none';
   }
-  document.querySelector('.card__section').innerHTML = adalaberCard;
+  document.querySelector('.adalaber__card').innerHTML = adalaberCard;
 }
 
 // call functions and events
-select.addEventListener('change', showAdalaberCard)
+select.addEventListener('change', showAdalaberCard);
 checkDataLocalStorage();
